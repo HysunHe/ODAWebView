7ce4c15b90886f2852e1c7d1925844ad539103b6
HysunHe <hysunhe@foxmail.com>
2019-11-18 15:57:32 +0800

    rollback to zxing for barcode

--- webviewApp/src/scanbar.js
+++ webviewApp/src/scanbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Quagga from 'quagga';
+// import Quagga from 'quagga';
+import { BrowserBarcodeReader } from '@zxing/library';
 import { postback } from './RestUtil';
 import './App.css';
 
@@ -21,53 +22,68 @@ class ScanBar extends Component {
 
     async componentDidMount() {
         console.log("*** Scan componentDidMount");
-        let _this = this;
-        Quagga.init({
-            inputStream : {
-                name : "Live",
-                type : "LiveStream"
-              },
-              decoder : {
-                readers : ["code_128_reader"]
-              }
-        }, function(err) {
-            if (err) {
-                console.log(err);
-                return false;
-            }
-            Quagga.start();
-        });
-        Quagga.onProcessed(function(result) {
-            var drawingCtx = Quagga.canvas.ctx.overlay,
-                drawingCanvas = Quagga.canvas.dom.overlay;
-    
-            if (result) {
-                if (result.boxes) {
-                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                    result.boxes.filter(function (box) {
-                        return box !== result.box;
-                    }).forEach(function (box) {
-                        Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-                    });
-                }
-    
-                if (result.box) {
-                    Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-                }
-    
-                if (result.codeResult && result.codeResult.code) {
-                    Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-                }
-            }
-        });
+        console.log("*** Scanning QR code");
+        const codeReader = new BrowserBarcodeReader();
+        let devices = await codeReader.getVideoInputDevices();
+        console.log("*** devices: " , devices);
 
-        Quagga.onDetected(function(result) {
-            console.log("*** Got result: ", result);
-            _this.confirmPay({
-                text: result.codeResult.code
-            });
-            Quagga.stop();
-        });
+        // Default
+        codeReader.decodeFromInputVideoDevice(null, 'video')
+            .then((result) => {
+                console.log("Decode result: ", result);
+                this.confirmPay(result);
+                 // this.inputAmt.focus();
+          }).catch((err) => {
+                console.error("Decode error:", err);
+          })
+
+        // let _this = this;
+        // Quagga.init({
+        //     inputStream : {
+        //         name : "Live",
+        //         type : "LiveStream"
+        //       },
+        //       decoder : {
+        //         readers : ["code_128_reader"]
+        //       }
+        // }, function(err) {
+        //     if (err) {
+        //         console.log(err);
+        //         return false;
+        //     }
+        //     Quagga.start();
+        // });
+        // Quagga.onProcessed(function(result) {
+        //     var drawingCtx = Quagga.canvas.ctx.overlay,
+        //         drawingCanvas = Quagga.canvas.dom.overlay;
+    
+        //     if (result) {
+        //         if (result.boxes) {
+        //             drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+        //             result.boxes.filter(function (box) {
+        //                 return box !== result.box;
+        //             }).forEach(function (box) {
+        //                 Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+        //             });
+        //         }
+    
+        //         if (result.box) {
+        //             Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+        //         }
+    
+        //         if (result.codeResult && result.codeResult.code) {
+        //             Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+        //         }
+        //     }
+        // });
+
+        // Quagga.onDetected(function(result) {
+        //     console.log("*** Got result: ", result);
+        //     _this.confirmPay({
+        //         text: result.codeResult.code
+        //     });
+        //     Quagga.stop();
+        // });
     }
 
     render() {
@@ -88,10 +104,10 @@ class ScanBar extends Component {
         if(this.state.showScan) {
             scanSection = (
                 <div style={{display: "flex",  justifyContent: "center",  alignItems: "center", marginTop: "20px"}}>
-                    {/* <video id="video" className="QrCode-Square" style={{width: "100%"}}></video> */}
-                    <section id="container" className="container">
+                    <video id="video" className="QrCode-Square" style={{width: "100%"}}></video>
+                    {/* <section id="container" className="container">
                         <div id="interactive" className="viewport"></div>
-                    </section>
+                    </section> */}
                 </div>
             )
         }
