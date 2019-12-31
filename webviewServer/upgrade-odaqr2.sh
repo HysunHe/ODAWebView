e10258b71ee7ac0d3d012d87b71b7581a39f4b48
Hysun He <hysunhe@foxmail.com>
2019-12-31 13:30:30 +0800

    impl

--- webviewServer/upgrade-odaqr2.sh
+++ webviewServer/upgrade-odaqr2.sh
@@ -11,10 +11,17 @@ git clone https://github.com/HysunHe/ODAWebView.git
 
 sleep 1
 
+echo "npm install webviewApp"
+cd /home/oracle/hysun-workspace/ODAWebView/webviewApp
+chmod +x *.sh
+npm install
+
+sleep 1
+
 echo "Producing new image..."
 cd /home/oracle/hysun-workspace/ODAWebView/webviewServer
 chmod +x *.sh
-
+npm install
 ./build-image.sh
 
 sleep 1
